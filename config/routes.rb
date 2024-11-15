3cbd96d4616994cc7c49185962626c6778ffa667
Will VanderWyden <vanderwyden@gmail.com>
2024-11-15 12:33:20 +0900

    ldf

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  root to: 'lists#index'
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
